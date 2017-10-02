19e59f12743dea8f0157fc0deb92aede21d53506
phi16 <phi16.256@gmail.com>
2017-10-02 17:35:45 +0900

    po

--- Lib/World.hs
+++ Lib/World.hs
@@ -67,7 +67,7 @@ normal :: Field a => World a -> World a
 normal p = normalize $ gradient p
 
 boundary :: Field a => World a -> a
-boundary p = max (abs $ p^.x) (abs $ p^.z) - 1-- sqrt ((p^.x)^2 + (p^.z)^2) - 1
+boundary p = sqrt ((p^.x)^2 + (p^.z)^2) - 1
 
 boundGradient :: Field a => World a -> World a
 boundGradient = grad boundary

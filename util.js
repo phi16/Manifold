59b7c4195ce55c403252669df0dfbcf0c0cdc579
phi16 <phi16.256@gmail.com>
2017-10-05 13:13:47 +0900

    linear, not nearest

--- util.js
+++ util.js
@@ -20,8 +20,8 @@ window.addEventListener("load",_=>{
   const worldTexture = gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D,worldTexture);
   gl.texImage2D(gl.TEXTURE_2D,0,gl.RGBA,scrW*2,scrH*2,0,gl.RGBA,gl.UNSIGNED_BYTE,null);
-  gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MAG_FILTER,gl.NEAREST);
-  gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MIN_FILTER,gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MAG_FILTER,gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MIN_FILTER,gl.LINEAR);
   gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_S,gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_T,gl.CLAMP_TO_EDGE);
   gl.framebufferTexture2D(gl.FRAMEBUFFER,gl.COLOR_ATTACHMENT0,gl.TEXTURE_2D,worldTexture,0);
